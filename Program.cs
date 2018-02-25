e0ec8012c0f5435cfe55115306bac3c75fd8a31b
enlightenedihi <enlightenihi@live.com>
2018-02-25 08:53:53 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS.Oid.DNA.Console
+{
+    
+}
+
+namespace NCA
+{
+    using NCL;
+    using NCE;
+
+    public interface IDNA
+    {
+        int DNA();
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CL _cl = new CL();
+            int j = ((NCL.IDNA)_cl).DNA();      //  
+
+            NCE.Program _pr = new NCE.Program();
+            int k = ((NCE.IDNA)_pr).DNA();
+        }
+    }
+}
